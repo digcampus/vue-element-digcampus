e2c8d2e41d1f2d5394543d2e086bc169a53e369b
zhangwei <1094660626@qq.com>
2019-02-26 13:35:33 +0800

    linechart

--- src/views/dashboard/admin/components/LineChart.vue
+++ src/views/dashboard/admin/components/LineChart.vue
@@ -81,7 +81,7 @@ export default {
     setOptions({ expectedData, actualData } = {}) {
       this.chart.setOption({
         title: {
-          text: '张三历史成绩',
+          text: '一年级期末考试',
           x: 'left'
         },
         tooltip: {
@@ -91,8 +91,8 @@ export default {
           }
         },
         legend: {
-          data: ['语文成绩', '数学成绩'],
-          selected: { '数学成绩': false } // 初始化时，不显示该曲线
+          data: ['最高成绩', '张三成绩', '李四成绩'],
+          selected: { '李四成绩': false } // 初始化时，不显示该曲线
         },
         toolbox: {
           show: true,
@@ -109,7 +109,7 @@ export default {
           {
             type: 'category',
             boundaryGap: false,
-            data: ['一年级期末考试', '二年级期末考试', '三年级期末考试', '四年级期末考试', '五年级期末考试', '六年级期末考试']
+            data: ['语文', '数学', '英语', '物理', '化学', '生物']
           }
         ],
         yAxis: [
@@ -119,13 +119,167 @@ export default {
         ],
         series: [
           {
-            name: '语文成绩',
+            name: '最高成绩',
+            type: 'line',
+            tiled: '总量',
+            data: [99, 99, 89, 88, 92, 90]
+          },
+          {
+            name: '张三成绩',
             type: 'line',
             tiled: '总量',
             data: [null, 80, 89, 76, 92, 84]
           },
           {
-            name: '数学成绩',
+            name: '李四成绩',
+            type: 'line',
+            tiled: '总量',
+            data: [80, 90, 89, null, 88, 92]
+          }
+        ]
+      })
+    },
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+      this.setOptions(this.chartData)
+    }
+  }
+}
+</script>
+<template>
+  <div :class="className" :style="{height:height,width:width}"/>
+</template>
+
+<script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+import { debounce } from '@/utils'
+
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '350px'
+    },
+    autoResize: {
+      type: Boolean,
+      default: true
+    },
+    chartData: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      chart: null,
+      sidebarElm: null
+    }
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler(val) {
+        this.setOptions(val)
+      }
+    }
+  },
+  mounted() {
+    this.initChart()
+    if (this.autoResize) {
+      this.__resizeHandler = debounce(() => {
+        if (this.chart) {
+          this.chart.resize()
+        }
+      }, 100)
+      window.addEventListener('resize', this.__resizeHandler)
+    }
+
+    // 监听侧边栏的变化
+    this.sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    this.sidebarElm && this.sidebarElm.addEventListener('transitionend', this.sidebarResizeHandler)
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    if (this.autoResize) {
+      window.removeEventListener('resize', this.__resizeHandler)
+    }
+
+    this.sidebarElm && this.sidebarElm.removeEventListener('transitionend', this.sidebarResizeHandler)
+
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    sidebarResizeHandler(e) {
+      if (e.propertyName === 'width') {
+        this.__resizeHandler()
+      }
+    },
+    setOptions({ expectedData, actualData } = {}) {
+      this.chart.setOption({
+        title: {
+          text: '一年级期末考试',
+          x: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          data: ['最高成绩', '张三成绩', '李四成绩'],
+          selected: { '李四成绩': false } // 初始化时，不显示该曲线
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: true },
+            magicType: { show: true, type: ['line', 'bar'] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['语文', '数学', '英语', '物理', '化学', '生物']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '最高成绩',
+            type: 'line',
+            tiled: '总量',
+            data: [99, 99, 89, 88, 92, 90]
+          },
+          {
+            name: '张三成绩',
+            type: 'line',
+            tiled: '总量',
+            data: [null, 80, 89, 76, 92, 84]
+          },
+          {
+            name: '李四成绩',
             type: 'line',
             tiled: '总量',
             data: [80, 90, 89, null, 88, 92]
