6bf3d67bcb729514809f2dac662940d6286c8273
zhangwei <1094660626@qq.com>
2019-03-20 19:20:16 +0800

    navbar

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -26,7 +26,6 @@
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
           <router-link to="/">
@@ -34,11 +33,11 @@
               {{ $t('navbar.dashboard') }}
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
+          <router-link to="/">
             <el-dropdown-item>
-              {{ $t('navbar.github') }}
+              用户设置
             </el-dropdown-item>
-          </a>
+          </router-link>
           <el-dropdown-item divided>
             <span style="display:block;" @click="logout">{{ $t('navbar.logOut') }}</span>
           </el-dropdown-item>
